80db6c3dcef32e893bdd65649e776c1624350e5a
Satyam Arora <satyamarora373@gmail.com>
2024-11-29 00:16:32 +0530

    added routing in Navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import MovieGrid from './components/MoviesGrid';
 import WatchList from './components/WatchList';
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { BrowserRouter as Router, Routes, Route,Link } from 'react-router';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Router>
+      <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
         <Routes>
           <Route path="/" element={<MovieGrid/>}></Route>
           <Route path="/watchlist" element={<WatchList/>}></Route>
