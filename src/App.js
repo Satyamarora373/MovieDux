0b64ab1c2c1c08353c3ab0dedd48916789e22b3b
Satyam Arora <satyamarora373@gmail.com>
2024-11-29 00:04:31 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import MovieGrid from './components/MoviesGrid';
 import WatchList from './components/WatchList';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 function App() {
   return (
     <div className="App">
       <Header/>
-      
       <Router>
-        <nav>
-          
-        </nav>
-
         <Routes>
           <Route path="/" element={<MovieGrid/>}></Route>
+          <Route path="/watchlist" element={<WatchList/>}></Route>
         </Routes>
       </Router>
       <Footer/>
