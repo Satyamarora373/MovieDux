bb9dc727f47589040ea00473f160711422d8df93
Satyam Arora <satyamarora373@gmail.com>
2024-11-29 14:55:31 +0530

    final changes

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,92 +1,113 @@
-import React,{useState,useEffect} from "react";
+import React, { useState } from "react";
+import "../styles.css";
 import MovieCard from "./MovieCard";
-export default function MovieGrid(){
-    const[movies,setMovies]=useState([]);
-    const [searchTerm, setSearchTerm] = new useState([]);
 
-    const[rating,setRating]=useState("All")
-    const[genre,setGenre]=useState("All Genres")
-    useEffect(()=>{
-        fetch("movies.json")
-        .then(response=>response.json())
-        .then(data=>setMovies(data))
-        // setMovies();
-    })
-    const handleSearchChange = (e) => {
-        setSearchTerm(e.target.value);
-    };
-    const handleGenreChange = (e) => {
-        setGenre(e.target.value);
-    };
-    const handleRatingChange = (e) => {
-        setRating(e.target.value);
-    };
-    const matchesGenre = (movie,genre)=>{
-        return genre==="All Genres" || movie.genre.toLowerCase() ===genre.toString().toLowerCase();
-    }
-    const matchesSearchTerm = (movie,searchTerm)=>{
-        return movie.title.toLowerCase().includes(searchTerm.toString().toLowerCase());
-    }
-    const matchesRating = (movie, rating)=>{
-        switch(rating){
-            case 'All':
-                return true;
-            case 'Good':
-                return movie.rating>=8;
-            case 'Ok':
-                return movie.rating>=5 && movie.rating<8;
-            case "Bad":
-                return movie.rating<5;
-            default:
-                return false;
-        }
-    }
-    const filteredmovies= movies.filter((movie)=>
-        matchesGenre(movie,genre) &&
-        matchesRating(movie,rating) &&
-        matchesSearchTerm(movie,searchTerm)
+export default function MoviesGrid({ movies, watchlist, toggleWatchlist }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [genre, setGenre] = useState("All Genres");
+  const [rating, setRating] = useState("All");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+  };
+
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
+  };
+
+  const matchesGenre = (movie, genre) => {
+    return (
+      genre === "All Genres" ||
+      movie.genre.toLowerCase() === genre.toLowerCase()
     );
+  };
 
-    
-    return(
-        <div>
-        <input 
+  const matchesSearchTerm = (movie, searchTerm) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const matchesRating = (movie, rating) => {
+    switch (rating) {
+      case "All":
+        return true;
+
+      case "Good":
+        return movie.rating >= 8;
+
+      case "Ok":
+        return movie.rating >= 5 && movie.rating < 8;
+
+      case "Bad":
+        return movie.rating < 5;
+
+      default:
+        return false;
+    }
+  };
+
+  const filteredMovies = movies.filter(
+    (movie) =>
+      matchesGenre(movie, genre) &&
+      matchesRating(movie, rating) &&
+      matchesSearchTerm(movie, searchTerm)
+  );
+
+  return (
+    <div>
+      <input
         type="text"
-        placeholder="Enter Title"
         className="search-input"
+        placeholder="Search movies..."
         value={searchTerm}
         onChange={handleSearchChange}
-        />
-        <div className="filter-bar">
-            <div className="filter-slot">
-                <label>Genre</label>
-                <select className="filter-dropdown" value={genre} onChange={handleGenreChange}>
-                    <option>All Genres</option>
-                    <option>Action</option>
-                    <option>Drama</option>
-                    <option>Fantasy</option>
-                    <option>Drama</option>
-                </select>
-            </div>
+      />
 
-            <div className="filter-slot">
-                <label>Rating</label>
-                <select className="filter-dropdown" value={rating} onChange={handleRatingChange}>
-                    <option>All</option>
-                    <option>Good</option>
-                    <option>Ok</option>
-                    <option>Bad</option>
-                </select>
-            </div>
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select
+            className="filter-dropdown"
+            value={genre}
+            onChange={handleGenreChange}
+          >
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
         </div>
 
-        <div className="movies-grid">
-            {
-                filteredmovies.map(movie=>(
-                    <MovieCard movie={movie} key={movie.id}/>
-                ))
-            }
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select
+            className="filter-dropdown"
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
         </div>
-        </div>
-    )   
+      </div>
+
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard
+            movie={movie}
+            key={movie.id}
+            toggleWatchlist={toggleWatchlist}
+            isWatchlisted={watchlist.includes(movie.id)}
+          ></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,11 +1,24 @@
 import React from "react";
-
+import MovieCard from "./MovieCard";
 import '../styles.css';
 
-export default function WatchList(){
+export default function WatchList({movies,watchlist,toggleWatchList}){
     return(
         <div>
-            <h1 className="title"> Your WatchList</h1>
-        </div>
+      <h1 className="title">Your Watchlist</h1>
+      <div className="watchlist">
+        {watchlist.map((id) => {
+          const movie = movies.find((movie) => movie.id === id);
+          return (
+            <MovieCard
+              key={id}
+              movie={movie}
+              toggleWatchList={toggleWatchList}
+              isWatchlisted={true}
+            ></MovieCard>
+          );
+        })}
+      </div>
+    </div>
     )
 }
